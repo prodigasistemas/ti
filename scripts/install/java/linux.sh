5c9c3b6da6eee76152cbf9eb9609392bfa17b758
Luiz Sanches <luizgrsanches@gmail.com>
2016-06-09 08:44:49 -0300

    improvements in download java

--- scripts/install/java/linux.sh
+++ scripts/install/java/linux.sh
@@ -44,6 +44,14 @@ menu () {
   echo $(eval dialog $_TITLE --stdout --menu \"$1\" 0 0 0 $2)
 }
 
+download_java () {
+  wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/$1-$2/jdk-$1-linux-$3"
+}
+
+delete_file () {
+  [ -e "$1" ] && rm $1
+}
+
 install_openJDK6 () {
   if [ $_OS_TYPE = "deb" ]; then
     _PACKAGE_NAME="openjdk-6-jdk"
@@ -68,11 +76,16 @@ install_oracleJava6 () {
     _JAVA_VERSION="6u45"
     _BINARY_VERSION="b06"
     _JAVA_FOLDER="jdk1.6.0_45"
-    wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/$_JAVA_VERSION-$_BINARY_VERSION/jdk-$_JAVA_VERSION-linux-$_ARCH.bin"
-    bash "jdk-$_JAVA_VERSION-linux-$_ARCH.bin"
+    _JAVA_FILE="jdk-$_JAVA_VERSION-linux-$_ARCH.bin"
+
+    download_java $_JAVA_VERSION $_BINARY_VERSION "$_ARCH.bin"
+
+    bash $_JAVA_FILE
     mv "$_JAVA_FOLDER" /usr/lib/jvm/
     chown -R root:root "/usr/lib/jvm/$_JAVA_FOLDER"
     ln -s "/usr/lib/jvm/$_JAVA_FOLDER" "/usr/lib/jvm/java-oracle-6"
+
+    delete_file $_JAVA_FILE
 }
 
 install_oracleJava7 () {
@@ -81,15 +94,21 @@ install_oracleJava7 () {
   _JAVA_FOLDER="jdk1.7.0_80"
 
   if [ $_OS_TYPE = "deb" ]; then
-    wget --no-cookies --no-check-certificate --header "Cookie:oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/$_JAVA_VERSION-$_BINARY_VERSION/jdk-$_JAVA_VERSION-linux-$_ARCH.tar.gz"
-    tar -xvzf "jdk-$_JAVA_VERSION-linux-$_ARCH.tar.gz"
+    download_java $_JAVA_VERSION $_BINARY_VERSION "$_ARCH.tar.gz"
+
+    _JAVA_FILE="jdk-$_JAVA_VERSION-linux-$_ARCH.tar.gz"
+    tar -xvzf $_JAVA_FILE
     mv "$_JAVA_FOLDER/" /usr/lib/jvm/
     chown -R root:root "/usr/lib/jvm/$_JAVA_FOLDER"
     ln -s "/usr/lib/jvm/$_JAVA_FOLDER" "/usr/lib/jvm/java-oracle-7"
   elif [ $_OS_TYPE = "rpm" ]; then
-    wget --no-cookies --no-check-certificate --header "Cookie:oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/$_JAVA_VERSION-$_BINARY_VERSION/jdk-$_JAVA_VERSION-linux-$_ARCH.rpm"
-    $_PACKAGE_COMMAND localinstall -y "jdk-$_JAVA_VERSION-linux-$_ARCH.rpm"
+    download_java $_JAVA_VERSION $_BINARY_VERSION "$_ARCH.rpm"
+
+    _JAVA_FILE="jdk-$_JAVA_VERSION-linux-$_ARCH.rpm"
+    $_PACKAGE_COMMAND localinstall -y $_JAVA_FILE
   fi
+
+  delete_file $_JAVA_FILE
 }
 
 install_oracleJava8 () {
@@ -98,15 +117,21 @@ install_oracleJava8 () {
   _JAVA_FOLDER="jdk1.8.0_92"
 
   if [ $_OS_TYPE = "deb" ]; then
-    wget --no-cookies --no-check-certificate --header "Cookie:oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/$_JAVA_VERSION-$_BINARY_VERSION/jdk-$_JAVA_VERSION-linux-$_ARCH.tar.gz"
-    tar -xvzf "jdk-$_JAVA_VERSION-linux-$_ARCH.tar.gz"
+    download_java $_JAVA_VERSION $_BINARY_VERSION "$_ARCH.tar.gz"
+
+    _JAVA_FILE="jdk-$_JAVA_VERSION-linux-$_ARCH.tar.gz"
+    tar -xvzf $_JAVA_FILE
     mv "$_JAVA_FOLDER/" /usr/lib/jvm
     chown -R root:root "/usr/lib/jvm/$_JAVA_FOLDER"
     ln -s "/usr/lib/jvm/$_JAVA_FOLDER" "/usr/lib/jvm/java-oracle-8"
   elif [ $_OS_TYPE = "rpm" ]; then
-    wget --no-cookies --no-check-certificate --header "Cookie:oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/$_JAVA_VERSION-$_BINARY_VERSION/jdk-$_JAVA_VERSION-linux-$_ARCH.rpm"
-    $_PACKAGE_COMMAND localinstall -y "jdk-$_JAVA_VERSION-linux-$_ARCH.rpm"
+    download_java $_JAVA_VERSION $_BINARY_VERSION "$_ARCH.rpm"
+
+    _JAVA_FILE="jdk-$_JAVA_VERSION-linux-$_ARCH.rpm"
+    $_PACKAGE_COMMAND localinstall -y $_JAVA_FILE
   fi
+
+  delete_file $_JAVA_FILE
 }
 
 main () {
@@ -116,11 +141,11 @@ main () {
 
   _ARCH=$(menu "Select the architecture" "$_ARCH_LIST")
 
-  [ -z "$_ARCH" ] && main
+  [ -z "$_ARCH" ] && exit 1
 
   _VERSION=$(menu "Select the version" "$_VERSION_LIST")
 
-  [ ! -z "$_ARCH" ] && install_$_VERSION
+  [ ! -z "$_VERSION" ] && install_$_VERSION
 }
 
 main
