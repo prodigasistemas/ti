19f468a61397b0e13ca224ba6b7f9066fdf2bea7
Luiz Sanches <lsanches@protonmail.com>
2018-12-28 15:43:48 -0300

    fixed restore

--- docs/scripts/install/restore/linux.sh
+++ docs/scripts/install/restore/linux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# set -x
 
 # https://aurelio.net/shell/dialog/#itensescolhidos
 # http://xmodulo.com/create-dialog-boxes-interactive-shell-script.html
@@ -6,7 +7,9 @@
 export _APP_NAME="Restore"
 _TOOLS_FOLDER="/opt/tools"
 _FOLDER="$_TOOLS_FOLDER/backup"
+_STORAGE_FOLDER="$_FOLDER/storage"
 _CONFIG_FILE="$_FOLDER/backup.conf"
+_LOG_FILE="/tmp/restore.log"
 _LOG_SYNC="$_FOLDER/logs/synchronizing.log"
 _OPTIONS_LIST="perform_sync 'Synchronize server with local folder' \
                select_files 'Select files from restore'"
@@ -25,6 +28,16 @@ setup () {
   [ -e "$_FUNCTIONS_FILE" ] && source $_FUNCTIONS_FILE && rm $_FUNCTIONS_FILE
 
   os_check
+}
+
+msg_log () {
+  _MSG=$1
+
+  echo "" >> "$_LOG_FILE"
+  echo "$_MSG" >> "$_LOG_FILE"
+  echo "" >> "$_LOG_FILE"
+
+  print_colorful yellow bold "$_MSG"
 }
 
 sync_rsync () {
@@ -94,11 +107,11 @@ perform_restore () {
     if [ -z "$(command -v "$_command_name")" ]; then
       message "Error" "$_command_name is not installed!"
     else
-      eval "$_command_line" >> "$_LOG_FILE" 2>> "$_LOG_FILE"
+      cp "$_STORAGE_FOLDER/$_RESTORE_FILE" "$_destination"
 
-      if [ $? -eq 0 ] && [ -e "/tmp/$_RESTORE_FILE_NAME" ]; then
-        cp "$_RESTORE_FILE" "$_destination"
-      fi
+      chown $_USER_LOGGED: "$_destination/$_RESTORE_FILE_NAME"
+
+      [ -e "$_destination/$_RESTORE_FILE_NAME" ] && eval "$_command_line" >> "$_LOG_FILE" 2>> "$_LOG_FILE"
     fi
   else
     _check_command=$(ssh -C -p "$_HOST_PORT" "$_access" "command -v $_command_name")
@@ -106,7 +119,7 @@ perform_restore () {
     if [ -z "$_check_command" ]; then
       message "Error" "$_command_name is not installed!"
     else
-      scp -C -P "$_HOST_PORT" "$_RESTORE_FILE" "$_access:$_destination" >> "$_LOG_FILE" 2>> "$_LOG_FILE"
+      scp -C -P "$_HOST_PORT" "$_STORAGE_FOLDER/$_RESTORE_FILE" "$_access:$_destination" >> "$_LOG_FILE" 2>> "$_LOG_FILE"
 
       if [ $? -eq 0 ]; then
         ssh -C -p "$_HOST_PORT" "$_access" "$_command_line" >> "$_LOG_FILE" 2>> "$_LOG_FILE"
@@ -117,30 +130,33 @@ perform_restore () {
 
 restore_folder () {
   _RESTORE_FOLDER=$(echo $_RESTORE_FILE | cut -d/ -f3)
-  _FOLDER_LIST=$(sed '/^folder:$_RESTORE_FOLDER/!d;' "$_HOST_FILE")
+  _RESTORE_FILE_NAME=$(echo $_RESTORE_FILE | cut -d/ -f4)
+  _FOLDER_LIST=$(sed "/^folder:$_RESTORE_FOLDER/ !d;" "$_HOST_FILE")
 
   if [ -n "$_FOLDER_LIST" ]; then
     _name=$(echo "$_FOLDER_LIST" | cut -d: -f2)
     _path=$(echo "$_FOLDER_LIST" | cut -d: -f3)
     _DEST="/tmp/tools/restore"
 
-    print_colorful yellow bold "> Decompressing $_RESTORE_FILE_NAME to $_HOST_ADDRESS"
+    msg_log "> Decompressing $_RESTORE_FILE_NAME to $_HOST_ADDRESS"
 
     _commands="mkdir -p $_DEST && \
-               mv /tmp/$_RESTORE_FILE_NAME $_DEST \
-               tar -xzf $_DEST/$_RESTORE_FILE_NAME && \
-               sudo rsync -rv $_DEST/$_path/ $_path/ && \
+               sudo chown $_USER_LOGGED: -R $_DEST && \
+               mv /tmp/$_RESTORE_FILE_NAME $_DEST && \
+               cd $_DEST && \
+               tar -xzf $_RESTORE_FILE_NAME && \
+               sudo rsync -rv $_DEST$_path/ $_path/ ; \
                sudo rm -rf $_DEST"
 
     perform_restore "tar" "/tmp" "$_commands"
   fi
 }
 
-backup_database () {
+restore_database () {
   _RESTORE_DB_TYPE=$(echo $_RESTORE_FILE | cut -d/ -f3)
   _RESTORE_DB_NAME=$(echo $_RESTORE_FILE | cut -d/ -f4)
   _RESTORE_FILE_NAME=$(echo $_RESTORE_FILE | cut -d/ -f5)
-  _DB_FIELDS=$(sed '/^database:$_RESTORE_DB_TYPE/!d; /$_RESTORE_DB_NAME/!d;' "$_HOST_FILE")
+  _DB_FIELDS=$(sed "/^database:$_RESTORE_DB_TYPE/ !d; /$_RESTORE_DB_NAME/ !d;" "$_HOST_FILE")
 
   if [ -n "$_DB_FIELDS" ]; then
     _DB_TYPE=$(echo "$_DB_FIELDS" | cut -d: -f2)
@@ -153,26 +169,28 @@ backup_database () {
     case $_DB_TYPE in
       mysql)
         _COMMAND=mysql
-        _CREATE_DB=""
-        _CREATE_ROLE=""
-        _DB_RESTORE="MYSQL_PWD=$_DB_PASS $_COMMAND -h $_DB_HOST -u $_DB_USER $_DB_NAME"
+        _CREATE_1="MYSQL_PWD=$_DB_PASS $_COMMAND -h $_DB_HOST -u $_DB_USER -e \"CREATE DATABASE $_RESTORE_DB_NAME;\""
+        _CREATE_2="MYSQL_PWD=$_DB_PASS $_COMMAND -h $_DB_HOST -u $_DB_USER -e \"CREATE USER '$_RESTORE_DB_NAME'@'$_DB_HOST' IDENTIFIED BY '$_RESTORE_DB_NAME';\" && \
+                   MYSQL_PWD=$_DB_PASS $_COMMAND -h $_DB_HOST -u $_DB_USER -e \"GRANT ALL PRIVILEGES ON $_RESTORE_DB_NAME.* TO '$_RESTORE_DB_NAME'@'$_DB_HOST' WITH GRANT OPTION;\" && \
+                   MYSQL_PWD=$_DB_PASS $_COMMAND -h $_DB_HOST -u $_DB_USER -e \"FLUSH PRIVILEGES;\""
+        _DB_RESTORE="MYSQL_PWD=$_DB_PASS $_COMMAND -h $_DB_HOST -u $_DB_USER $_RESTORE_DB_NAME"
         ;;
 
       postgresql)
         _COMMAND=psql
-        _CREATE_ROLE="PGPASSWORD=$_DB_PASS $_COMMAND -h $_DB_HOST -U $_DB_USER -c \"CREATE ROLE $_RESTORE_DB_NAME LOGIN ENCRYPTED PASSWORD '$_RESTORE_DB_NAME' NOINHERIT VALID UNTIL 'infinity';\""
-        _CREATE_DB="PGPASSWORD=$_DB_PASS $_COMMAND -h $_DB_HOST -U $_DB_USER -c \"CREATE DATABASE $_RESTORE_DB_NAME WITH OWNER=$_RESTORE_DB_NAME ENCODING='UTF8';\""
-        _DB_RESTORE="PGPASSWORD=$_DB_PASS $_COMMAND -h $_DB_HOST -U $_DB_USER -d $_DB_NAME"
+        _CREATE_1="PGPASSWORD=$_DB_PASS $_COMMAND -h $_DB_HOST -U $_DB_USER -c \"CREATE ROLE $_RESTORE_DB_NAME LOGIN ENCRYPTED PASSWORD '$_RESTORE_DB_NAME' NOINHERIT VALID UNTIL 'infinity';\""
+        _CREATE_2="PGPASSWORD=$_DB_PASS $_COMMAND -h $_DB_HOST -U $_DB_USER -c \"CREATE DATABASE $_RESTORE_DB_NAME WITH OWNER=$_RESTORE_DB_NAME ENCODING='UTF8';\""
+        _DB_RESTORE="PGPASSWORD=$_DB_PASS $_COMMAND -h $_DB_HOST -U $_DB_USER -d $_RESTORE_DB_NAME"
         ;;
     esac
 
-    print_colorful yellow bold "> Restoring $_DB_TYPE database $_DB_NAME to $_HOST_ADDRESS"
+    msg_log "> Restoring $_DB_TYPE database $_RESTORE_DB_NAME to $_HOST_ADDRESS"
 
-    _commands="tar -xzf /tmp/$_RESTORE_FILE_NAME && \
-               $_CREATE_ROLE && \
-               $_CREATE_DB && \
-               $_DB_RESTORE < /tmp/$_SQL_FILE && \
-               rm /tmp/$_SQL_FILE*"
+    _commands="gunzip /tmp/$_RESTORE_FILE_NAME && \
+               $_CREATE_1 && \
+               $_CREATE_2 && \
+               $_DB_RESTORE < /tmp/$_SQL_FILE ; \
+               rm /tmp/$_SQL_FILE"
 
     perform_restore "$_COMMAND" "/tmp" "$_commands"
   fi
@@ -186,7 +204,7 @@ select_host () {
   _HOSTS_LIST="$_FOLDER/hosts.list"
 
   if [ -e "$_HOSTS_LIST" ]; then
-    _HOST_FIELDS=$(sed '/^ *$/d; /^ *#/d; /^$_RESTORE_HOST/ !d;' $_HOSTS_LIST)
+    _HOST_FIELDS=$(egrep "^$_RESTORE_HOST" "$_HOSTS_LIST")
 
     if [ -n "$_HOST_FIELDS" ]; then
       _HOST_NAME=$(echo "$_HOST_FIELDS" | cut -d: -f1)
@@ -196,7 +214,6 @@ select_host () {
 
       _HOST_FILE="$_FOLDER/hosts/$_HOST_NAME.list"
       _HOST_FOLDER="$_FOLDER/storage/$_HOST_NAME"
-      _LOG_FILE="$_FOLDER/logs/$_HOST_NAME.log"
 
       if [ -e "$_HOST_FILE" ]; then
         case $_RESTORE_TYPE in
@@ -213,15 +230,15 @@ select_host () {
 }
 
 select_files () {
-  _STORAGE=$_FOLDER/storage/
+  _LIST=""
 
-  for _list in $(ls -R $_STORAGE | grep :)
+  for _list in $(ls -R $_STORAGE_FOLDER | grep :)
   do
     _dir=$(echo $_list | cut -d: -f1)
 
     _file=$(ls -r $_dir/*.gz 2> /dev/null | head -n 1)
 
-    [ -n "$_file" ] && _LIST+="${_file/$_STORAGE/} '' off "
+    [ -n "$_file" ] && _LIST+="${_file/$_STORAGE_FOLDER\//} '' off "
   done
 
   if [ -z "$_LIST" ]; then
@@ -230,15 +247,32 @@ select_files () {
     _LIST+="3>&1 1>&2 2>&3"
   fi
 
-  _FILES=$(checklist "Restaurar" "Quais arquivos?" "$_LIST")
+  _FILES=$(checklist "Restore" "What files?" "$_LIST")
+
+  [ -z "$_FILES" ] && main
 
   confirm "Do you confirm restore from selected items?"
   [ $? -eq 1 ] && main
+
+  [ -e "$_LOG_FILE" ] && rm "$_LOG_FILE"
+
+  msg_log "[$(date +"%Y-%m-%d %H:%M:%S")] Start restore"
 
   for _file in $_FILES
   do
     select_host $_file
   done
+
+  if [ -e "$_LOG_FILE" ]; then
+    _RESTORE_LOG="$_FOLDER/logs/restore.log"
+
+    cat "$_LOG_FILE" >> $_RESTORE_LOG
+    chown $_USER_LOGGED: $_RESTORE_LOG
+  fi
+
+  textbox $_LOG_FILE
+
+  [ -e "$_LOG_FILE" ] && rm "$_LOG_FILE"
 
   main
 }
